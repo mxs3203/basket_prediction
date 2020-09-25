573c5cb4fe362da218d31ba7a9ee3bde3a071043
mxs3203 <mxs3203@g.rit.edu>
2020-09-26 01:48:33 +0200

    overfitted model with 72%

--- training.py
+++ training.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional,Flatten
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
@@ -10,15 +10,21 @@ import tensorflow as tf
 import os
 import glob
 
-os.environ['TF_CPP_MIN_LOG_LEVEL']='1'
-
 gpu = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(gpu[0], True)
 
-
-early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=3)
-checkpoints = tf.keras.callbacks.ModelCheckpoint("output/", monitor='val_loss', verbose=1, save_best_only=False,save_weights_only=False, mode='auto', save_freq='epoch')
-
+def scheduler(epoch, lr):
+  if epoch < 25:
+    return lr
+  else:
+    return lr * tf.math.exp(-0.1)
+early_stop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=20)
+checkpoints = tf.keras.callbacks.ModelCheckpoint("output/", monitor='val_loss', verbose=1, save_best_only=False,save_weights_only=False, mode='auto', save_freq='epoch',period = 10)
+schedule = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose=1)
+BATCH_SIZE = 2048
+EPOCHS = 300
+OPTIMIZER = tf.keras.optimizers.RMSprop(learning_rate=0.001)
+# OPTIMIZER = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 class PlotLosses(tf.keras.callbacks.Callback):
     def on_train_begin(self, logs={}):
@@ -57,7 +63,7 @@ def tokenise_words(inputs):
 
 
     print("Example Input: ")
-    print(sentances[11])
+    print(sentances[1])
 
 
     corpus = sentances
@@ -75,8 +81,10 @@ def tokenise_words(inputs):
             input_sequences.append(n_gram_sequence)
 
     # pad sequences
-    max_sequence_len = max([len(x) for x in input_sequences])
-    input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre'))
+    max_sequence_len = np.mean(np.array(([len(x) for x in input_sequences])))
+    max_sequence_len = int(max_sequence_len)
+    print("Cutoff sequences: ",max_sequence_len)
+    input_sequences = np.array(pad_sequences(input_sequences, maxlen=max_sequence_len, padding='pre',truncating='pre'))
     # create predictors and label
     predictors, label = input_sequences[:, :-1], input_sequences[:, -1]
 
@@ -86,19 +94,27 @@ def tokenise_words(inputs):
 
 def make_model(total_words, max_sequence_len):
     model = Sequential()
-    model.add(Embedding(total_words, 50, input_length=max_sequence_len-1))
-    model.add(Bidirectional(LSTM(50, return_sequences=True)))
-    model.add(Dropout(0.5))
-    model.add(LSTM(100))
+    model.add(Embedding(total_words, 2000, input_length=max_sequence_len-1))
+    model.add(Bidirectional(LSTM(1500,  return_sequences=True)))
+    model.add(Bidirectional(LSTM(800, return_sequences=True)))
+    #model.add(Bidirectional(LSTM(100, return_sequences=True)))
+    model.add(LSTM(400))
+    #model.add(Flatten())
+    model.add(Dropout(0.35))
+    model.add(Dense(total_words/2, activation='relu'))
+    model.add(Dropout(0.35))
     model.add(Dense(total_words/2, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(total_words, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='categorical_accuracy')
+    model.compile(loss='categorical_crossentropy',
+                  optimizer=OPTIMIZER,
+                  metrics='categorical_accuracy')
+    print(model.summary())
     return model
 
 
-def model_fit(model, predictors, label, epoch=100):
-    history = model.fit(predictors, label,epochs=epoch, verbose=1, batch_size=128, callbacks=[early_stop, checkpoints, plot_losses], validation_split=0.2)
+def model_fit(model, predictors, label, epoch):
+    history = model.fit(predictors, label,epochs=epoch, verbose=1, batch_size=BATCH_SIZE, callbacks=[early_stop, checkpoints, plot_losses,schedule], validation_split=0.2)
     return history
 
 def make_plot(history):
@@ -116,9 +132,9 @@ def make_plot(history):
 def main():
     result = glob.glob('data/*.csv')
     inputs = result
-    predictors, label, max_sequence_len, total_words,tokenizer, sentences = tokenise_words(inputs)
+    predictors, label, max_sequence_len, total_words, tokenizer, sentences = tokenise_words(inputs)
     model = make_model(total_words, max_sequence_len)
-    history = model_fit(model, predictors, label, epoch =300)
+    history = model_fit(model, predictors, label, epoch=EPOCHS)
     print()
     print()
     for i in sentences[1:10]:
